08b16af005f980283f53c465daf5ebeafb29e855
Jacob Haslehurst <jacob@haslehurst.net>
2018-07-14 15:20:30 +1000

    rename to audio_metadata

--- examples/parse_flac.rs
+++ examples/parse_flac.rs
@@ -1,6 +1,6 @@
-extern crate audio_metadata_parser;
+extern crate audio_metadata;
 
-use audio_metadata_parser::*;
+use audio_metadata::*;
 
 fn main() {
     let f = open_file("/Users/Jacob/Desktop/11 - Slipped.flac").unwrap();
